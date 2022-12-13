42a6614be4f0bd291ddf16830a1c295a050291e3
Jhonatan Alves <jhonatan4alves@gmail.com>
2022-12-12 23:05:29 -0300

    feat: finishing app

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,16 @@
-import { Box } from "@mantine/core";
+import { Box, List, ThemeIcon } from "@mantine/core";
+import { CheckCircleFillIcon } from "@primer/octicons-react";
 import { useState } from "react";
 import useSWR from "swr";
 import "./App.css";
 import AddTodo from "./components/AddTodo";
+
+export interface Todo {
+  id: number;
+  title: string;
+  body: string;
+  done: boolean;
+}
 
 export const ENDPOINT = "http://localhost:4000";
 
@@ -10,12 +18,49 @@ const fetcher = (URL: string) =>
   fetch(`${ENDPOINT}/${URL}`).then((res) => res.json());
 
 function App() {
-  const { data, mutate } = useSWR("api/todos", fetcher);
+  const { data, mutate } = useSWR<Todo[]>("api/todos", fetcher);
+
+  async function markTodoAsDone(id: number) {
+    const updated = await fetch(`${ENDPOINT}/api/todo/${id}/done`, {
+      method: "PATCH",
+    }).then((r) => r.json());
+
+    mutate(updated);
+  }
 
   return (
-    <Box>
-      {JSON.stringify(data)}
-      <AddTodo />
+    <Box
+      sx={(theme) => ({
+        padding: "2rem",
+        width: "100%",
+        maxWidth: "40rem",
+        margin: "0 auto",
+      })}
+    >
+      <List spacing="xs" size="sm" mb={12} center>
+        {data?.map((todo) => {
+          return (
+            <List.Item
+              onClick={() => markTodoAsDone(todo.id)}
+              key={`todo_list_${todo.id}`}
+              icon={
+                todo.done ? (
+                  <ThemeIcon color="teal" size={24} radius="xl">
+                    <CheckCircleFillIcon size={20} />
+                  </ThemeIcon>
+                ) : (
+                  <ThemeIcon color="gray" size={24} radius="xl">
+                    <CheckCircleFillIcon size={20} />
+                  </ThemeIcon>
+                )
+              }
+            >
+              {todo.title}
+            </List.Item>
+          );
+        })}
+      </List>
+      <AddTodo mutate={mutate} />
     </Box>
   );
 }
